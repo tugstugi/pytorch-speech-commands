33fe30d4443e591e97962821ee813724ba6cc22e
Erdene-Ochir Tuguldur <tugstugi@gmail.com>
2018-01-20 17:21:52 +0100

    rename dataset into datasets

--- download_speech_commands_dataset.sh
+++ download_speech_commands_dataset.sh
@@ -3,15 +3,15 @@ set -e
 
 FILE_NAME=speech_commands_v0.01.tar.gz
 URL=http://download.tensorflow.org/data/$FILE_NAME
-DATASET_FOLDER=dataset/speech_commands
+DATASET_FOLDER=datasets/speech_commands
 
 echo "downloading $URL...\n"
-wget -O dataset/$FILE_NAME $URL
+#wget -O datasets/$FILE_NAME $URL
 
 echo "extracting $FILE_NAME..."
 TEMP_DIRECTORY=$DATASET_FOLDER/audio
 mkdir -p $TEMP_DIRECTORY
-tar -xzf dataset/$FILE_NAME -C $TEMP_DIRECTORY
+tar -xzf datasets/$FILE_NAME -C $TEMP_DIRECTORY
 
 echo "splitting the dataset into train, validation and test sets..."
 python $DATASET_FOLDER/split_dataset.py $DATASET_FOLDER
