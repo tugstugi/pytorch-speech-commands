6e44f8257d382221e6b04c0bf8636ff50541fc96
Erdene-Ochir Tuguldur <tugstugi@gmail.com>
2018-01-25 20:28:35 +0100

    simplify model creations

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,41 @@ from .dpn import *
 from .resnet import *
 from .densenet import *
 from .resnext import *
+
+available_models = [
+    'vgg19_bn',
+    'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152',
+    'wideresnet28_10', 'wideresnet28_10D', 'wideresnet52_10',
+    'resnext29_8_64',
+    'dpn92',
+    'densenet_bc_250_24', 'densenet_bc_190_40'
+]
+
+def create_model(model_name, num_classes, in_channels):
+    if model_name == "resnet18":
+        model = resnet18(num_classes=num_classes, in_channels=in_channels)
+    if model_name == "resnet34":
+        model = resnet34(num_classes=num_classes, in_channels=in_channels)
+    elif model_name == "resnet50":
+        model = resnet50(num_classes=num_classes, in_channels=in_channels)
+    elif model_name == "resnet101":
+        model = resnet101(num_classes=num_classes, in_channels=in_channels)
+    elif model_name == "resnet152":
+        model = resnet152(num_classes=num_classes, in_channels=in_channels)
+    elif model_name == "wideresnet28_10":
+        model = WideResNet(depth=28, widen_factor=10, dropRate=0, num_classes=num_classes, in_channels=in_channels)
+    elif model_name == "wideresnet28_10D":
+        model = WideResNet(depth=28, widen_factor=10, dropRate=0.3, num_classes=num_classes, in_channels=in_channels)
+    elif model_name == "wideresnet52_10":
+        model = WideResNet(depth=52, widen_factor=10, dropRate=0, num_classes=num_classes, in_channels=in_channels)
+    elif model_name == "resnext29_8_64":
+        model = CifarResNeXt(nlabels=num_classes, in_channels=in_channels)
+    elif model_name == "dpn92":
+        model = DPN92(num_classes=num_classes, in_channels=in_channels)
+    elif model_name == "densenet_bc_250_24":
+        model = DenseNet(depth=250, growthRate=24, compressionRate=2, num_classes=num_classes, in_channels=in_channels)
+    elif model_name == "densenet_bc_190_40":
+        model = DenseNet(depth=190, growthRate=40, compressionRate=2, num_classes=num_classes, in_channels=in_channels)
+    else:
+        model = vgg19_bn(num_classes=num_classes, in_channels=in_channels)
+    return model
--- train_cifar10.py
+++ train_cifar10.py
@@ -34,7 +34,7 @@ parser.add_argument("--lr-scheduler-step-size", type=int, default=50, help='lr s
 parser.add_argument("--lr-scheduler-gamma", type=float, default=0.1, help='learning rate is multiplied by the gamma to decrease it')
 parser.add_argument("--max-epochs", type=int, default=150, help='max number of epochs')
 parser.add_argument("--resume", type=str, help='checkpoint file to resume')
-parser.add_argument("--model", choices=['vgg19_bn', 'wideresnet28_10', 'wideresnet28_10D', 'wideresnet52_10', 'dpn92', 'densenet_100_12', 'densenet_bc_190_40'], default='vgg19_bn', help='model of NN')
+parser.add_argument("--model", choices=models.available_models, default=models.available_models[0], help='model of NN')
 args = parser.parse_args()
 
 use_gpu = torch.cuda.is_available()
@@ -65,23 +65,7 @@ full_name = '%s_%s_%s_bs%d_lr%.1e_wd%.1e' % (args.model, args.optim, args.lr_sch
 if args.comment:
     full_name = '%s_%s' % (full_name, args.comment)
 
-in_channels = 3
-if args.model == "wideresnet28_10":
-    model = models.WideResNet(depth=28, widen_factor=10, dropRate=0, num_classes=len(CLASSES), in_channels=in_channels)
-elif args.model == "wideresnet28_10D":
-    model = models.WideResNet(depth=28, widen_factor=10, dropRate=0.3, num_classes=len(CLASSES), in_channels=in_channels)
-elif args.model == "wideresnet52_10":
-    model = models.WideResNet(depth=52, widen_factor=10, dropRate=0, num_classes=len(CLASSES), in_channels=in_channels)
-elif args.model == "resnext29_8_64":
-    model = models.CifarResNeXt(nlabels=len(CLASSES), in_channels=in_channels)
-elif args.model == "dpn92":
-    model = models.DPN92(num_classes=len(CLASSES), in_channels=in_channels)
-elif args.model == "densenet_100_12":
-    model = models.DenseNet(depth=190, growthRate=40, compressionRate=2, num_classes=len(CLASSES), in_channels=in_channels)
-elif args.model == "densenet_bc_190_40":
-    model = models.DenseNet(depth=190, growthRate=40, compressionRate=2, num_classes=len(CLASSES), in_channels=in_channels)
-else:
-    model = models.vgg19_bn(num_classes=len(CLASSES), in_channels=in_channels)
+model = models.create_model(model_name=args.model, num_classes=len(CLASSES), in_channels=3)
 
 if use_gpu:
     model = torch.nn.DataParallel(model).cuda()
