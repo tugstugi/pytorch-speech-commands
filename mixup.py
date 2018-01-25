a9a6a16ba65a34c0d63e52a55f528d0c2747226b
Erdene-Ochir Tuguldur <tugstugi@gmail.com>
2018-01-25 21:15:36 +0100

    fix wrong ported code

--- mixup.py
+++ mixup.py
@@ -12,21 +12,21 @@ __all__ = [ 'mixup_cross_entropy_loss', 'mixup' ]
 
 import numpy as np
 import torch
+from torch.autograd import Variable
 
-class CrossEntropyLoss(object):
-    def __call__(self, input, target, size_average=True):
-        """Origin: https://github.com/moskomule/mixup.pytorch
-        in PyTorch's cross entropy, targets are expected to be labels
-        so to predict probabilities this loss is needed
-        suppose q is the target and p is the input
-        loss(p, q) = -\sum_i q_i \log p_i
-        """
-        assert input.size() == target.size()
-        assert isinstance(input, Variable) and isinstance(target, Variable)
-        input = torch.log(torch.nn.functional.softmax(input, dim=1).clamp(1e-5, 1))
-        # input = input - torch.log(torch.sum(torch.exp(input), dim=1)).view(-1, 1)
-        loss = - torch.sum(input * target)
-        return loss / input.size()[0] if size_average else loss
+def mixup_cross_entropy_loss(input, target, size_average=True):
+    """Origin: https://github.com/moskomule/mixup.pytorch
+    in PyTorch's cross entropy, targets are expected to be labels
+    so to predict probabilities this loss is needed
+    suppose q is the target and p is the input
+    loss(p, q) = -\sum_i q_i \log p_i
+    """
+    assert input.size() == target.size()
+    assert isinstance(input, Variable) and isinstance(target, Variable)
+    input = torch.log(torch.nn.functional.softmax(input, dim=1).clamp(1e-5, 1))
+    # input = input - torch.log(torch.sum(torch.exp(input), dim=1)).view(-1, 1)
+    loss = - torch.sum(input * target)
+    return loss / input.size()[0] if size_average else loss
 
 def onehot(targets, num_classes):
     """Origin: https://github.com/moskomule/mixup.pytorch
@@ -44,8 +44,9 @@ def mixup(inputs, targets, num_classes, alpha=2):
     weight = torch.Tensor(np.random.beta(alpha, alpha, s))
     index = np.random.permutation(s)
     x1, x2 = inputs, inputs[index, :, :, :]
-    y1, y2 = onehot(targets), onehot(targets[index,])
+    y1, y2 = onehot(targets, num_classes), onehot(targets[index,], num_classes)
     weight = weight.view(s, 1, 1, 1)
     inputs = weight*x1 + (1-weight)*x2
     weight = weight.view(s, 1)
     targets = weight*y1 + (1-weight)*y2
+    return inputs, targets
